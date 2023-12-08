05063f0330573e6b26df21254a2e1ff2a04e9f76
anvo <songun1120@gamil.com>
2023-12-09 02:12:48 +0900

    커뮤니티 페이지 생성

---
+++ app/api/posts/route.js
@@ -0,0 +1,29 @@
+import connectDB from '@/libs/database'
+import Post from '@/models/post'
+import { NextResponse } from 'next/server'
+
+export async function POST(request) {
+  const { postId, title, videoUrl, mainText } = await request.json()
+  const youtubeUrl = videoUrl
+    .replace('https://youtu.be/', '')
+    .replace('youtu.be/', '')
+    .replace('https://www.youtube.com/watch?v=', '')
+    .replace('www.youtube.com/watch?v=', '')
+    .replace('youtube.com/watch?v=', '')
+
+  await connectDB()
+  await Post.create({
+    postId,
+    title,
+    videoUrl: youtubeUrl,
+    mainText,
+    comments: [null],
+  })
+  return NextResponse.json({}, { status: 201 })
+}
+
+export async function GET(request) {
+  await connectDB()
+  const posts = await Post.find()
+  return NextResponse.json({ posts }, { status: 200 })
+}
